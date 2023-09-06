9ae5f36b8e6bd94dad7a591a04bdfd534bef3bb9
itsmeken20 <123226533+itsmeken20@users.noreply.github.com>
2023-09-06 20:40:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+
+
+
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+     let letters = word.textContent.split("");
+     word.textContent="";
+     letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+}); 
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i * 80);
+    });
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+letter.className = "letter behind";
+setTimeout(()=>{
+    letter.className = "letter in";
+    },340 + i * 80);
+});
+currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText,3000)
+
+// mix it up portfolio section
+
+var mixer = mixitup('.portfolio-gallery');
+
+
+//----------SCROLL BAR-------------////
+
+let menuLi =document.querySelectorAll('hearder ul li a');
+let section =document.querySelectorAll('section');
+
+
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuli[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+
+//------STICKY NAV BAR-----------------////
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
